effef56cd644fc6b39d4255beaf3ff29a3ef41b3
VuChau <vu.chauthanh@asnet.com.vn>
2019-01-31 09:33:43 +0700

    Init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Handling %+v\n", r)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello world !"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	port := ":8000"
+	fmt.Printf("Starting to service on port %s\n", port)
+	http.ListenAndServe(port, nil)
+}
