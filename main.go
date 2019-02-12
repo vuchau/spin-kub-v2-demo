1061df6a8b0158925f80874d904728b4c5d3df48
Vu Chau <vu.chauthanh@asnet.com.vn>
2019-02-12 16:07:49 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,6 +14,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", index)
 	port := ":8000"
-	fmt.Printf(" Starting to service on port %s\n", port)
+	fmt.Printf("Starting to service on port %s\n", port)
 	http.ListenAndServe(port, nil)
 }
