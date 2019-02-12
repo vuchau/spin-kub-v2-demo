c1de3533385e5160068cc586a791f94ad98156c9
VuChau <vu.chauthanh@asnet.com.vn>
2019-02-12 18:20:56 +0700

    test failed

--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    total := Sum(5, 5)
-    if total != 10 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-    }
+	total := Sum(5, 1)
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
 }
